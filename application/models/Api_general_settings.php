c63ea7b968d77158dd58d4a98a2cd476adae2131
Firman <firman766hi@gmail.com>
2020-02-16 22:49:14 +0700

    registrasi

---
+++ application/models/Api_general_settings.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Api_general_settings extends CI_Model
+{
+    public function __construct(){
+        parent::__construct();
+        
+        $this->tabel = 'general_settings';
+    }
+
+
+    public function getEmailVerivication()
+    {
+        $this->db->select('email_verification');
+        $query = $this->db->get($this->tabel);
+        $result = $query->row()->email_verification;
+        return $result;
+    }
+
+    public function getValueOf($data)
+    {
+        $this->db->select($data);
+        $query = $this->db->get($this->tabel);
+        $result = $query->row()->$data;
+        return $result;
+    }
+
+}
