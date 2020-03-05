c83e0c4abd275a5bc667109f221da2a3fea1aa2c
Firman <firman766hi@gmail.com>
2020-03-05 21:26:18 +0700

    api profile : follow-unfollow

---
+++ application/models/Api_profile_model.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Api_profile_model extends CI_Model
+{
+    public function __construct(){
+        parent::__construct();
+        
+    }
+
+    //follow user
+    public function follow_unfollow_user($data)
+    {
+        $follow = $this->get_follow($data["following_id"], $data["follower_id"]);
+        if (empty($follow)) {
+            //add follower
+            $this->db->insert('followers', $data);
+        } else {
+            $this->db->where('id', $follow->id);
+            $this->db->delete('followers');
+        }
+    }
+
+    public function get_follow($following_id, $follower_id)
+    {
+        $following_id = clean_number($following_id);
+        $follower_id = clean_number($follower_id);
+        $this->db->where('following_id', $following_id);
+        $this->db->where('follower_id', $follower_id);
+        $query = $this->db->get('followers');
+        return $query->row();
+    }
+
+}
